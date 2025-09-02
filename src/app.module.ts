c24534608689a4f849220a8b2ec773cea188d7ff
Meghana <rmeghana184@gmail.com>
2025-09-02 10:46:31 +0530

    added config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
@@ -6,10 +7,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     BlogsModule,
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@miraki.lkjmada.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=miraki`,
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get('MONGO_USER')}:${configService.get('MONGO_PASS')}@miraki.lkjmada.mongodb.net/${configService.get('MONGO_DB')}?retryWrites=true&w=majority&appName=miraki`,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,23 +1,28 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateBlogDto {
-    @IsString()
-    @IsNotEmpty()
-    title:string
-    @IsString()
-    @IsNotEmpty()
-    content:string
-    @IsString()
-    @IsNotEmpty()
-    author:string
-    @IsString()
-    @IsOptional()
-    email:string
-    @IsString()
-    @IsOptional()
-    @MaxLength(10)
-    @MinLength(10)
-    phonenumber:string
-
-
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+  @IsString()
+  @IsOptional()
+  email: string;
+  @IsString()
+  @IsOptional()
+  @MaxLength(10)
+  @MinLength(10)
+  phonenumber: string;
 }
